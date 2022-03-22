a184cdbfac27d0ba5d9edfa06bcd9215a3fa23c2
pStrachota <p.strachota52@gmail.com>
2022-03-22 19:58:02 +0100

    add CircleCommand class for future bindings

---
+++ CSHARP_PW_PROJECT/Commands/CircleCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CSHARP_PW_PROJECT.Commands
+{
+    internal class CircleCommands : ICommand
+    {
+        private readonly Func<object, bool> _canExecuteAction;
+        private readonly Action<object> _executeAction;
+        public CircleCommands(Action<object> executeAction, Func<object, bool> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+        public void InvokeCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public event EventHandler? CanExecuteChanged;
+        public bool CanExecute(object? parameter) => _canExecuteAction?.Invoke(parameter) ?? true;
+        public void Execute(object? parameter) => _executeAction(parameter);
+    }
+}
