215203a7b339a715e4ea32abcca25bc5c115fccc
pStrachota <p.strachota52@gmail.com>
2022-03-22 19:49:38 +0100

    add Circle model class and remove unused imports

---
+++ CSHARP_PW_PROJECT/Model/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+
+namespace CSHARP_PW_PROJECT.Model
+{
+    public class Circle
+    {
+        public int height { get; set; }
+        public int wide { get; set; }
+        public string color { get; set; }
+        public int leftPosition { get; set; }
+        public int topPosition { get; set; }
+        public TranslateTransform RenderTransform { get; internal set; }
+    }
+}
