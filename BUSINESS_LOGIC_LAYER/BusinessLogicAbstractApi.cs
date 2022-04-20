51cbe1e2b2fb9c385aff93b21f265a71892e19fe
pStrachota <p.strachota52@gmail.com>
2022-04-20 20:18:34 +0200

    add business logic layer

---
+++ BUSINESS_LOGIC_LAYER/BusinessLogicAbstractApi.cs
@@ -0,0 +1,20 @@
+﻿using DATA_LAYER;
+
+namespace BUSINESS_LOGIC_LAYER;
+
+public abstract class BusinessLogicAbstractApi
+{
+    public static BusinessLogicAbstractApi CreateLayer(DataLayerAbstractApi data = default(DataLayerAbstractApi))
+    {
+        return new BusinessLogic(data == null ? DataLayerAbstractApi.CreateLinq2Sql() : data);
+    }
+    
+    private class BusinessLogic : BusinessLogicAbstractApi
+    {
+        public BusinessLogic(DataLayerAbstractApi dataLayerAPI)
+        {
+            MyDataLayer = dataLayerAPI;
+        }
+        private readonly DataLayerAbstractApi MyDataLayer;
+    }
+}
