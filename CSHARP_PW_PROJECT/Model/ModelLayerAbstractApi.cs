c21a4b0dc3eef0369a187103d26e8566faad6b6e
pStrachota <p.strachota52@gmail.com>
2022-04-20 20:29:29 +0200

    add model layer class

---
+++ CSHARP_PW_PROJECT/Model/ModelLayerAbstractApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using BUSINESS_LOGIC_LAYER;
+
+namespace CSHARP_PW_PROJECT.Model;
+
+public class ModelLayerAbstractApi
+{
+    public static ModelLayerAbstractApi CreateAPI(BusinessLogicAbstractApi _businessLogicAbstractApi = default)
+    {
+        return new ModelLayer(_businessLogicAbstractApi ?? BusinessLogicAbstractApi.CreateLayer());
+    }
+
+    public class ModelLayer : ModelLayerAbstractApi
+    {
+        public ModelLayer(BusinessLogicAbstractApi businessLogicAbstractApi)
+        {
+            _businessLogicAbstractApi = businessLogicAbstractApi;
+        }
+
+        private readonly BusinessLogicAbstractApi _businessLogicAbstractApi;
+    }
+}
