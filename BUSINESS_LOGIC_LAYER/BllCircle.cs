315006fbb0496c05e667c57a75b8981001555884
pStrachota <p.strachota52@gmail.com>
2022-04-20 20:21:15 +0200

    add helper circle class for business logic layer

---
+++ BUSINESS_LOGIC_LAYER/BllCircle.cs
@@ -0,0 +1,28 @@
+﻿namespace BUSINESS_LOGIC_LAYER;
+
+public class BllCircle
+{
+    public BllCircle(int wide, int height)
+    {
+        Random random = new();
+        int toMoveHorizontal = random.Next(55, 1414);
+        int toMoveVertical = random.Next(275, 770 - height + 4);
+
+        this.wide = wide;
+        this.height = height;
+        this.startingLeftPosition = toMoveHorizontal;
+        this.startingTopPosition = toMoveVertical;
+        this.leftPosition = toMoveHorizontal;
+        this.topPosition = toMoveVertical;
+    }
+
+    private int height { get; set; }
+    private int wide { get; set; }
+    private string color { get; set; }
+    private int startingLeftPosition { get; set; }
+    private int startingTopPosition { get; set; }
+    private int lastLeftPosition { get; set; }
+    private int lastTopPosition { get; set; }
+    private int leftPosition { get; set; }
+    private int topPosition { get; set; }
+}
