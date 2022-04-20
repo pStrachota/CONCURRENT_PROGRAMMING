c7aedb3642e79ca472654618e4e3c6ae648c5833
pStrachota <p.strachota52@gmail.com>
2022-04-20 20:11:46 +0200

    add data layer

---
+++ DATA_LAYER/DataLayerAbstractApi.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+
+namespace DATA_LAYER;
+
+public abstract class DataLayerAbstractApi
+{
+    public static string ConString = "SAMPLE_CONNECTION_STRING";
+    
+    public abstract DataTable? ReadCircleDetails();
+    public static DataLayerAbstractApi CreateLinq2Sql()
+    {
+        return new Linq2Sql();
+    }
+
+    private class Linq2Sql : DataLayerAbstractApi
+    {
+        public override DataTable? ReadCircleDetails()
+        {
+            return null;
+        }
+    }
+}
