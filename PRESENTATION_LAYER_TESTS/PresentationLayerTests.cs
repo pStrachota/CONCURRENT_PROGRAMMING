bfcf63de14f666a84a393538f97f8911825d618c
pStrachota <p.strachota52@gmail.com>
2022-05-12 15:32:55 +0200

    rebuild business logic and presentation tests

---
+++ PRESENTATION_LAYER_TESTS/PresentationLayerTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using ViewModel;
+
+namespace PRESENTATION_TESTS
+{
+    [TestClass]
+    public class PresentationLayerTests
+    {
+        CircleViewModel _circleViewModel;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _circleViewModel = new();
+        }
+
+        [TestMethod]
+        public void CheckIf_PresentationLayerIsNotNull_AfterInitialization()
+        {
+            Assert.IsNotNull(_circleViewModel);
+
+        }
+
+        [TestMethod]
+        public void CheckIf_CircleListIsNotNull_AfterCVMInitialization()
+        {
+            Assert.IsNotNull(_circleViewModel.Circles);
+        }
+
+        [TestMethod]
+        public void CheckIf_CannotMoveCircles_IfInputDataIsWrong()
+        {
+            _circleViewModel.CircleRadiusMax = "incorrect text";
+            Assert.IsFalse(_circleViewModel.CanMoveCirclesCommand());
+
+        }
+
+    }
+}
