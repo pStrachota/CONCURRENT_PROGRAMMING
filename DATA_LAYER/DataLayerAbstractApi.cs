f21fd3eade39a00a4ad6f438b74a594296d210c2
pStrachota <p.strachota52@gmail.com>
2022-06-02 21:45:15 +0200

    add serilog logger to data layer

--- DATA_LAYER/DataLayerAbstractApi.cs
+++ DATA_LAYER/DataLayerAbstractApi.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using Serilog;
+using Serilog.Formatting.Compact;
+using System.Data;
 
 namespace DATA_LAYER;
 
@@ -7,9 +9,27 @@ public abstract class DataLayerAbstractApi
     public int BOX_WIDTH = 1445;
     public int BOX_HEIGHT = 504;
 
+    private static ILogger _logger;
+
+    public abstract ILogger GetLogger();
     public abstract List<IDLCircle> GetDllCirclesFromBox(int numberOfBalls, int minRadius, int maxRadius, int speed);
     public static DataLayerAbstractApi? CreateLinq2DLCircles()
     {
+        _logger = new LoggerConfiguration()
+               .WriteTo.Async(a => a.Debug())
+               .WriteTo.Async(a => a.MSSqlServer(
+                   connectionString: "Data Source=DESKTOP-NAMFMUD;Initial Catalog=CircleDiagnosticData; Integrated Security=True",
+                   sinkOptions: new Serilog.Sinks.MSSqlServer.MSSqlServerSinkOptions
+                   {
+                       TableName = "Logs",
+                       AutoCreateSqlTable = true
+                   }))
+               .WriteTo.Async(a => a.File(
+                   new RenderedCompactJsonFormatter(), @"C:\Users\lenovo\Desktop\LOGI\logJson.json"))
+               .WriteTo.Async(a => a.File(
+                   @"C:\Users\lenovo\Desktop\LOGI\logTxt.txt"))
+               .CreateLogger();
+
         return new Linq2DLCircles();
     }
 
@@ -18,6 +38,11 @@ public abstract class DataLayerAbstractApi
         public override List<IDLCircle> GetDllCirclesFromBox(int numberOfBalls, int minRadius, int maxRadius, int speed)
         {
             return new Box(BOX_WIDTH, BOX_HEIGHT, numberOfBalls, minRadius, maxRadius, speed).GetDllCircles();
+        }
+
+        public override ILogger GetLogger()
+        {
+            return _logger;
         }
     }
 }
